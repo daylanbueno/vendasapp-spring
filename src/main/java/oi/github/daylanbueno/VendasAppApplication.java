5efc403aed9349741acc08185d5e362f5ef8f1ce
DAILAN BUENO DOS SANTOS <x04501348143@tcu.gov.br>
2021-04-13 09:58:08 -0300

    criando consultas, update e delete  com jdbc template uso de mapper repository

--- src/main/java/oi/github/daylanbueno/VendasAppApplication.java
+++ src/main/java/oi/github/daylanbueno/VendasAppApplication.java
@@ -11,6 +11,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @SpringBootApplication
 @RestController
 public class VendasAppApplication {
@@ -30,6 +32,22 @@ public class VendasAppApplication {
             clienteRepository.salva(new Cliente("Amanda Cristina"));
             clienteRepository.salva(new Cliente("Dilan Bueno Rodrigues"));
 
+            List<Cliente> clientes = clienteRepository.obterTodos();
+            clientes.forEach(System.out::println);
+
+            clientes.forEach( c -> {
+                c.setNome(c.getNome() + " atualizado");
+                clienteRepository.atualiza(c);
+            });
+            System.out.println("clientes atualziados");
+            clientes.forEach(System.out::println);
+
+            System.out.println("recuperando amanda");
+            clienteRepository.obterPorNome("Amanda").forEach(System.out::println);
+
+            System.out.println("clientes deletando por id");
+            clienteRepository.deletar(1);
+            System.out.println("NOVA LISTA SEM O CLIENTE 1");
             clienteRepository.obterTodos().forEach(System.out::println);
 
         };
