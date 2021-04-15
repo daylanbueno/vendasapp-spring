0188e68f210127387eb6b2d71fd12dc9f47ad674
DAILAN BUENO DOS SANTOS <x04501348143@tcu.gov.br>
2021-04-15 08:01:29 -0300

    Fazendo mapeamento da entidade de produto..

--- src/main/java/oi/github/daylanbueno/domain/entity/Produto.java
+++ src/main/java/oi/github/daylanbueno/domain/entity/Produto.java
@@ -1,10 +1,25 @@
 package oi.github.daylanbueno.domain.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import java.math.BigDecimal;
 
+@Entity
+@Table(name = "PRODUTO")
 public class Produto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
     private Integer id;
+
+    @Column(name = "descricao", length = 100)
     private String descricao;
+
+    @Column(name = "preco_unitario")
     private BigDecimal preco;
 
     public Integer getId() {
