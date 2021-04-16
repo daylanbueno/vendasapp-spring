d76daa0db9100ef20098f3f0e0bb73f7631e4927
DAILAN BUENO DOS SANTOS <x04501348143@tcu.gov.br>
2021-04-16 09:45:41 -0300

    implentacao de endpointer para salvar novo cliente no banco de dados

--- src/main/java/oi/github/daylanbueno/VendasAppApplication.java
+++ src/main/java/oi/github/daylanbueno/VendasAppApplication.java
@@ -1,24 +1,12 @@
 package oi.github.daylanbueno;
 
-import oi.github.daylanbueno.domain.entity.Cliente;
-import oi.github.daylanbueno.domain.repository.ClienteRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class VendasAppApplication {
-
-    @Bean
-    CommandLineRunner executar(@Autowired ClienteRepository clienteRepository) {
-        return args -> {
-            clienteRepository.save(new Cliente("Dailan Bueno dos Santos"));
-        };
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(VendasAppApplication.class, args);
--- src/main/java/oi/github/daylanbueno/domain/controller/ClienteController.java
+++ src/main/java/oi/github/daylanbueno/domain/controller/ClienteController.java
@@ -5,6 +5,8 @@ import oi.github.daylanbueno.domain.repository.ClienteRepository;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,11 +24,18 @@ public class ClienteController {
     }
 
     @GetMapping("/{id}")
+    @ResponseBody
     public ResponseEntity<Cliente> obterClientePorId(@PathVariable Integer id) {
         Optional<Cliente> clienteOptional = clienteRepository.findById(id);
         if(clienteOptional.isPresent()) {
             return ResponseEntity.ok(clienteOptional.get());
         }
         return ResponseEntity.notFound().build();
+    }
+
+    @PostMapping
+    @ResponseBody
+    public ResponseEntity<Cliente> salvarCliente(@RequestBody Cliente cliente) {
+        return ResponseEntity.ok(clienteRepository.save(cliente));
     }
 }
